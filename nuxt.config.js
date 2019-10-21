131f59c4a5c055b3855c2b0f71985eaf60c7650c
khoatbui <khoatbui.dev@gmail.com>
2019-10-21 23:48:09 +0700

    Them cac thu vien UI

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,6 +57,16 @@ module.exports = {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      config.module.rules.push({
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        exclude: /(node_modules)/,
+        options: {
+          fix: true
+        }
+      })
+    }
   }
 }
