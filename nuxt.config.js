30e35400cfae50ed6de051fead75a185de59b99b
khoatbui <khoatbui.dev@gmail.com>
2019-11-13 00:47:44 +0700

    Thay doi port

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   mode: 'universal',
   server: {
-    port: 8000, // default: 3000
-    host: '0.0.0.0', // default: localhost,
+    port: 3000, // default: 3000
+    host: 'localhost', // default: localhost,
     timing: false,
   },
   serverMiddleware: ['~/api/index'],
