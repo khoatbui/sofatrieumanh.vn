dfbe6b1eb83e0f1161ac1828737c1cbddd02c0a5
khoatbui <khoatbui.dev@gmail.com>
2019-11-19 02:01:16 +0700

    Them property

--- components/v.2/ProductListComponent.vue
+++ components/v.2/ProductListComponent.vue
@@ -171,77 +171,83 @@
           </div>
         </div>
         <div class="product__list">
-          <div
-            v-for="(product, index) in productList"
-            :key="index + 'product'"
-            class="card border-0 product__card cursor--pointer"
-          >
-            <div class="card-body mp--none p-2">
-              <div
-                class="product__image"
-                :style="`background-image:url('${product.images[0].path}')`"
-              >
-                <div class="product__property">
-                  <span v-if="product.isHot" class="custom__badge custom__hot"
-                    >Hot</span
+          <div class="row mp--none">
+            <div
+              v-for="(product, index) in productList"
+              :key="index + 'product'"
+              class="col-12 col-sm-6 col-md-3 col-lg-3"
+            >
+              <div class="card border-0 product__card cursor--pointer">
+                <div class="card-body mp--none p-2">
+                  <div
+                    class="product__image"
+                    :style="`background-image:url('${product.images[0].path}')`"
                   >
-                  <span
-                    v-if="product.isNewProduct"
-                    class="custom__badge custom__new"
-                    >Sản phẩm mới</span
-                  >
-                  <span
-                    v-if="product.isSaleOff"
-                    class="custom__badgecustom__saleoff"
-                    >SaleOff</span
-                  >
-                </div>
-                <div class="product__action animated flipInX">
-                  <vs-button
-                    color="#333333"
-                    type="flat"
-                    class="py-1 px-2 addtocart__btn"
-                    to="/san-pham"
-                    >ADD TO CARD</vs-button
-                  >
-                  <div>
-                    <vs-button color="#333333" type="flat" class="py-1 px-2"
-                      ><i class="material-icons">
-                        favorite_border
-                      </i></vs-button
-                    >
-                    <vs-button color="#333333" type="flat" class="py-1 px-2"
-                      ><i class="material-icons">
-                        compare_arrows
-                      </i></vs-button
-                    >
+                    <div class="product__property">
+                      <span
+                        v-if="product.isHot"
+                        class="custom__badge custom__hot"
+                        >Hot</span
+                      >
+                      <span
+                        v-if="product.isNewProduct"
+                        class="custom__badge custom__new"
+                        >Sản phẩm mới</span
+                      >
+                      <span
+                        v-if="product.isSaleOff"
+                        class="custom__badgecustom__saleoff"
+                        >SaleOff</span
+                      >
+                    </div>
+                    <div class="product__action animated flipInX">
+                      <vs-button
+                        color="#333333"
+                        type="flat"
+                        class="py-1 px-2 addtocart__btn"
+                        to="/san-pham"
+                        >ADD TO CARD</vs-button
+                      >
+                      <div>
+                        <vs-button color="#333333" type="flat" class="py-1 px-2"
+                          ><i class="material-icons">
+                            favorite_border
+                          </i></vs-button
+                        >
+                        <vs-button color="#333333" type="flat" class="py-1 px-2"
+                          ><i class="material-icons">
+                            compare_arrows
+                          </i></vs-button
+                        >
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div class="product__info">
-                <div class="product__name my-2">
-                  {{ product.productName }}
-                </div>
-                <div class="product__detail my-2">
-                  <strong class="product__price"
-                    >{{
-                      new Intl.NumberFormat('vi-VN', {
-                        style: 'currency',
-                        currency: 'VND',
-                        minimumFractionDigits: 0,
-                      }).format(product.price)
-                    }}
-                    vnd</strong
-                  >
-                  <span class="product__star">
-                    <i
-                      v-for="i in product.star"
-                      :key="i"
-                      class="material-icons"
-                    >
-                      star_border
-                    </i>
-                  </span>
+                  <div class="product__info">
+                    <div class="product__name my-2">
+                      {{ product.productName }}
+                    </div>
+                    <div class="product__detail my-2">
+                      <strong class="product__price"
+                        >{{
+                          new Intl.NumberFormat('vi-VN', {
+                            style: 'currency',
+                            currency: 'VND',
+                            minimumFractionDigits: 0,
+                          }).format(product.price)
+                        }}
+                        vnd</strong
+                      >
+                      <span class="product__star">
+                        <i
+                          v-for="i in product.star"
+                          :key="i"
+                          class="material-icons"
+                        >
+                          star_border
+                        </i>
+                      </span>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
