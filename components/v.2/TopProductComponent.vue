6625b1d7060be7d000bd0be2e445735af0498cc7
khoatbui <khoatbui.dev@gmail.com>
2019-11-20 00:04:47 +0700

    Fix loi image

--- components/v.2/TopProductComponent.vue
+++ components/v.2/TopProductComponent.vue
@@ -27,7 +27,12 @@
                   <div class="card-body mp--none p-2">
                     <div
                       class="product__image"
-                      :style="`background-image:url('${pro.images[0].path}')`"
+                      :style="
+                        typeof pro.images === 'undefined' ||
+                        pro.images.length === 0
+                          ? `background-image:url('/images/product/1-01.jpg')`
+                          : `background-image:url('${pro.images[0].path}')`
+                      "
                     >
                       <div class="product__property">
                         <span v-if="pro.isHot" class="custom__badge custom__hot"
@@ -107,7 +112,12 @@
                   <div class="card-body mp--none p-2">
                     <div
                       class="product__image"
-                      :style="`background-image:url('${pro.images[0].path}')`"
+                      :style="
+                        typeof pro.images === 'undefined' ||
+                        pro.images.length === 0
+                          ? `background-image:url('/images/product/1-01.jpg')`
+                          : `background-image:url('${pro.images[0].path}')`
+                      "
                     >
                       <div class="product__property">
                         <span v-if="pro.isHot" class="custom__badge custom__hot"
@@ -187,7 +197,12 @@
                   <div class="card-body mp--none p-2">
                     <div
                       class="product__image"
-                      :style="`background-image:url('${pro.images[0].path}')`"
+                      :style="
+                        typeof pro.images === 'undefined' ||
+                        pro.images.length === 0
+                          ? `background-image:url('/images/product/1-01.jpg')`
+                          : `background-image:url('${pro.images[0].path}')`
+                      "
                     >
                       <div class="product__property">
                         <span v-if="pro.isHot" class="custom__badge custom__hot"
@@ -268,10 +283,10 @@
                     <div
                       class="product__image"
                       :style="
-                        typeof pro.images[0] === 'undefined' ||
+                        typeof pro.images === 'undefined' ||
                         pro.images.length === 0
-                          ? `background-image:url('${pro.images[0].path}')`
-                          : `background-image:url('/images/product/1-01.jpg')`
+                          ? `background-image:url('/images/product/1-01.jpg')`
+                          : `background-image:url('${pro.images[0].path}')`
                       "
                     >
                       <div class="product__property">
