a8cca5a7d329ca044b9cb4067e995d876edffe51
BUI VAN KHOAT <39306945+khoatbui@users.noreply.github.com>
2019-11-25 16:33:50 +0700

    Them order

---
+++ api/controllers/order.controller.js
@@ -0,0 +1,303 @@
+const mongoose = require('mongoose');
+const { check, validationResult } = require('express-validator');
+const Product = require('../models/product.model');
+
+module.exports.getAllProducts = function(req, res) {
+  Product.find()
+    .select({
+      productName: 1,
+      _id: 1,
+      createDate: 1,
+      modifyDate: 1,
+      isHot: 1,
+      isActive: 1,
+    })
+    .exec((error, response) => {
+      if (error) {
+        return next(error);
+      } else {
+        res.status(200).json(response);
+      }
+    });
+};
+
+module.exports.getAllProductsList = function(req, res) {
+  try {
+    Product.find({ isActive: true })
+      .select({
+        productName: 1,
+        price: 1,
+        oldPrice: 1,
+        _id: 1,
+        images: 1,
+        isNewProduct: 1,
+        isSaleOff: 1,
+        createDate: 1,
+        isHot: 1,
+        url: 1,
+      })
+      .exec((error, response) => {
+        if (error) {
+          return next(error);
+        } else {
+          res.status(200).json(response);
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports.getAllProductsListByNew = function(req, res) {
+  try {
+    Product.find({ isActive: true, isNewProduct: true })
+      .select({
+        productName: 1,
+        price: 1,
+        oldPrice: 1,
+        _id: 1,
+        createDate: 1,
+        isHot: 1,
+        images: 1,
+        isNewProduct: 1,
+        isSaleOff: 1,
+        url: 1,
+      })
+      .exec((error, response) => {
+        if (error) {
+          return next(error);
+        } else {
+          res.status(200).json(response);
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports.getAllProductsListBySaleOff = function(req, res) {
+  try {
+    Product.find({ isActive: true, isSaleOff: true })
+      .select({
+        productName: 1,
+        price: 1,
+        oldPrice: 1,
+        _id: 1,
+        createDate: 1,
+        isHot: 1,
+        isNewProduct: 1,
+        images: 1,
+        isSaleOff: 1,
+        url: 1,
+      })
+      .exec((error, response) => {
+        if (error) {
+          return next(error);
+        } else {
+          res.status(200).json(response);
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports.getAllProductsListByHot = function(req, res) {
+  try {
+    Product.find({ isActive: true, isHot: true })
+      .select({
+        productName: 1,
+        price: 1,
+        oldPrice: 1,
+        _id: 1,
+        createDate: 1,
+        isHot: 1,
+        isNewProduct: 1,
+        images: 1,
+        isSaleOff: 1,
+        url: 1,
+      })
+      .exec((error, response) => {
+        if (error) {
+          return next(error);
+        } else {
+          res.status(200).json(response);
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports.getAllProductsListByTrend = function(req, res) {
+  try {
+    Product.find({ isActive: true, isHot: true, isNewProduct: true })
+      .select({
+        productName: 1,
+        price: 1,
+        oldPrice: 1,
+        _id: 1,
+        createDate: 1,
+        isHot: 1,
+        isNewProduct: 1,
+        isSaleOff: 1,
+        images: 1,
+        url: 1,
+      })
+      .exec((error, response) => {
+        if (error) {
+          return next(error);
+        } else {
+          res.status(200).json(response);
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports.getAllProductsListByCategory = function(req, res) {
+  try {
+    Product.find({
+      isActive: true,
+      category: { $elemMatch: { url: req.params.id } },
+    })
+      .select({
+        productName: 1,
+        price: 1,
+        oldPrice: 1,
+        _id: 1,
+        createDate: 1,
+        isHot: 1,
+        url: 1,
+        images: 1,
+      })
+      .exec((error, response) => {
+        if (error) {
+          return next(error);
+        } else {
+          res.status(200).json(response);
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports.getAllProductsListBySearch = function(req, res) {
+  try {
+    Product.find({
+      productName: { $regex: req.params.keyword },
+      isActive: true,
+    })
+      .select({
+        productName: 1,
+        price: 1,
+        oldPrice: 1,
+        _id: 1,
+        createDate: 1,
+        isHot: 1,
+        url: 1,
+        images: 1,
+      })
+      .exec((error, response) => {
+        if (error) {
+          return next(error);
+        } else {
+          res.status(200).json(response);
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports.getAllProductsListByCategoryId = function(req, res) {
+  try {
+    Product.find({
+      isActive: true,
+      category: { $elemMatch: { _id: req.params.id } },
+    })
+      .select({
+        productName: 1,
+        price: 1,
+        oldPrice: 1,
+        _id: 1,
+        createDate: 1,
+        isHot: 1,
+        url: 1,
+        images: 1,
+      })
+      .exec((error, response) => {
+        if (error) {
+          return next(error);
+        } else {
+          res.status(200).json(response);
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports.getSingleProduct = function(req, res, next) {
+  Product.findById(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(200).json(data);
+    }
+  });
+};
+module.exports.getSingleProductByUrl = function(req, res, next) {
+  Product.findOne({ url: req.params.id }, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(200).json(data);
+    }
+  });
+};
+module.exports.insertProduct = function(req, res, next) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).jsonp(errors.array());
+  } else {
+    const product = new Product(req.body);
+    product
+      .save()
+      .then(response => {
+        res.status(201).json({
+          message: 'Product successfully created!',
+          result: response,
+        });
+      })
+      .catch(error => {
+        res.status(500).json({
+          error,
+        });
+      });
+  }
+};
+
+module.exports.updateProduct = function(req, res, next) {
+  Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (error, data) => {
+      if (error) {
+        return next(error);
+      } else {
+        res.json(data);
+      }
+    }
+  );
+};
+
+module.exports.deleteProduct = function(req, res, next) {
+  Product.findByIdAndRemove(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(200).json({
+        msg: data,
+      });
+    }
+  });
+};
---
+++ api/routers/order.router.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const controller = require('../controllers/product.controller');
+
+const router = express.Router();
+
+router.get('/', controller.getAllProducts);
+router.get('/product-list', controller.getAllProductsList);
+router.get(
+  '/product-list-by-category/:id',
+  controller.getAllProductsListByCategory
+);
+router.get(
+  '/product-list-by-search/:keyword',
+  controller.getAllProductsListBySearch
+);
+router.get(
+  '/product-list-by-category-id/:id',
+  controller.getAllProductsListByCategoryId
+);
+router.get('/product-list-by-state/new', controller.getAllProductsListByNew);
+router.get(
+  '/product-list-by-state/saleoff',
+  controller.getAllProductsListBySaleOff
+);
+router.get('/product-list-by-state/hot', controller.getAllProductsListByHot);
+router.get(
+  '/product-list-by-state/trend',
+  controller.getAllProductsListByTrend
+);
+
+router.post(
+  '/register-product',
+  [
+    check('productName')
+      .not()
+      .isEmpty()
+      .isLength({ min: 3 })
+      .withMessage('ProductName must be atleast 3 characters long'),
+  ],
+  controller.insertProduct
+);
+
+// Get Single  Users
+router.get('/single-product/:id', controller.getSingleProduct);
+// Get Single  Product by URL
+router.get('/single-product-by-url/:id', controller.getSingleProductByUrl);
+// Update User
+router.put('/update-product/:id', controller.updateProduct);
+
+// Delete User
+router.delete('/delete-product/:id', controller.deleteProduct);
+module.exports = router;
