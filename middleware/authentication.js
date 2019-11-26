5fe7c4cdb6776a0d81c1b8f2b51700e30ca00812
BUI VAN KHOAT <39306945+khoatbui@users.noreply.github.com>
2019-11-26 14:12:40 +0700

    Xoa consso

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,10 +1,7 @@
 export default function({ store, redirect }) {
   // If the user is not authenticated
   if (!store.state.login.loginStatus) {
-    console.log(store.state.login.loginStatus);
     return redirect('/admin/login');
   } else {
-    console.log(store.state.login.loginStatus);
-    console.log('Da dang nhap thanh cong');
   }
 }
